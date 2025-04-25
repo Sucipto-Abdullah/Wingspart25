21aac080094a202f12c36bbc58d9c006a59f640f
Sucipto-Abdullah <ciptoabdul23@gmail.com>
2025-04-25 15:53:52 +0700

    #

--- web/web-element/navigation.php
+++ web/web-element/navigation.php
@@ -13,9 +13,14 @@
             <button class="notification-button" style="grid-area: box-4;" onclick="showNotification()"><img src="icon/notification-icon.svg" alt="notification"></button>
             <button class="profile-button" style="grid-area: box-5;" onclick="showProfile()"><img src="icon/Profile picture icon default.svg" alt="profile-picture" id="profile-picture"></button>
         </nav>
-
-        <div class="navigation sub-notification open">
-            <h1>cok</h1>
+        <div class="sub-notification open">
+            <div class="notification-list">
+                <a href="#" id="notification-link">
+                    <img src="image/Product 1.png" class="notification-image" style="grid-area: image;">
+                    <h1 class="notification-header" style="grid-area: header;">Judul Notifikasi</h1>
+                    <p class="notification-text" style="grid-area: text;">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </a>
+            </div>
         </div>
     </body>
 </html>
