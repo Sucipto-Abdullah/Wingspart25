65aca6a5bc7944500712d9e61b6d72d6e92c305e
Sucipto-Abdullah <ciptoabdul23@gmail.com>
2025-05-02 08:23:36 +0700

    #

---
+++ web/web-element/profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>Profile pengguna</h1>
+    <ul>
+        <li><?= $account['username'] ?></li>
+        <li><?= $account['email'] ?></li>
+        <li><?= $account['phone-number'] ?></li>
+        <li><?= $account['address'] ?></li>
+    </ul>
+    <P>Belum ku desain halaman ini</P>
+</body>
+</html>
